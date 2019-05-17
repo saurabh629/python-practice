9a8baf7e57182d7bdd322ce422f93859159bf169
saurabh629 <saurabh629@gmail.com>
2019-05-17 13:22:08 -0400

    Update comments

--- combine_val_dict.py
+++ combine_val_dict.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 '''
-Python program to combine values in python list of dictionaries.
+Python program to combine values of the same keys in python list of dictionaries.
+e.g. 
+    [{'item': 'item1', 'amount': 400}, 
+          {'item': 'item2', 'amount': 300}, 
+          {'item': 'item1', 'amount': 750},
+          {'item': 'item2', 'amount': 900}]
+          
+    result=[{'item2': 1200, 'item1': 1150}]
 '''
 
 from collections import Counter

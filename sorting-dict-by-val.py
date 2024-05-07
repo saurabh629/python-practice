de555273e3e696263af9e2b141a3ace21e5d4491
saurabh629 <saurabh629@gmail.com>
2024-05-07 06:31:46 -0500

    Update sorting-dict-by-val.py

--- sorting-dict-by-val.py
+++ sorting-dict-by-val.py
@@ -7,11 +7,11 @@
 def sort_dict_by_value_ascending(dict):
     print ("Sorting the dict by values in ascending order")
     for key,value in sorted(dict.items(), key=lambda kv: kv[1]):
-        print ("{0}: {1}".format(key, value))
+        print (f"{key} : {value}")
 def sort_dict_by_value_descending(dict):
     print ("Sorting the dict by values in descending order")
     for key,value in sorted(dict.items(), key=lambda kv: kv[1], reverse=True):
-        print ("{0}: {1}".format(key, value))
+        print (f"{key} : {value}")
 
 def main():
    my_dict = {'coins':7, 'pens':6, 'cups':5, 'bags':1, 'bottles':4, 'books':5}
